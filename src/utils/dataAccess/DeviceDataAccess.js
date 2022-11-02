eb4eb2f7e90b7cb6afa0e38ff616fe34ea4191c2
Zakaria Bellal Bellahouel <ZakariaBellalBellahouel@eaton.com>
2022-11-02 14:47:10 -0400

    Debug Lines

--- src/utils/dataAccess/DeviceDataAccess.js
+++ src/utils/dataAccess/DeviceDataAccess.js
@@ -15,6 +15,24 @@ export function useGetDeviceInformation() {
                 console.log("I'm inside the get")
                 console.log(response.data)
             })
+            .catch(function (error) {
+                if (error.response) {
+                  // Request made and server responded
+                  console.log("Inside error.response");
+                  console.log(error.response.data);
+                  console.log(error.response.status);
+                  console.log(error.response.headers);
+                } else if (error.request) {
+                    console.log("Inside error.request");
+
+                  // The request was made but no response was received
+                  console.log(error.request);
+                } else {
+                    console.log("Inside error.Message");
+                  // Something happened in setting up the request that triggered an Error
+                  console.log('Error', error.message);
+                }
+            })
     }, [getDeviceInformationUrl]);
     return (deviceinformations);
 
