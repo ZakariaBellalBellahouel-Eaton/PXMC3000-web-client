32c36157fd65939c8c17a406c6e52aaa9a2118c4
Zakaria Bellal Bellahouel <ZakariaBellalBellahouel@eaton.com>
2022-11-02 17:49:23 -0400

    Add debug lines

--- src/utils/dataAccess/DeviceDataAccess.js
+++ src/utils/dataAccess/DeviceDataAccess.js
@@ -13,26 +13,40 @@ export function useGetDeviceInformation() {
             .then(response => {
                 setDeviceInformation(response.data)
                 console.log("I'm inside the get")
-                console.log(response.data)
+                console.log("This is the Data : ", response.data)
+                console.log("This is the Header : ", response.headers)
+                console.log("This is the Status : ", response.status)
+                console.log("This is the Status Text : ", response.statusText)
+                console.log("This is the Status config : ", response.config)
+                console.log("This is the Status Request : ", response.request)
+
             })
             .catch(function (error) {
+                console.log("I'm inside the catch")
                 if (error.response) {
-                  // Request made and server responded
-                  console.log("Inside error.response");
-                  console.log(error.response.data);
-                  console.log(error.response.status);
-                  console.log(error.response.headers);
+                    // Request made and server responded
+                    console.log("Inside error.response");
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
                 } else if (error.request) {
                     console.log("Inside error.request");
 
-                  // The request was made but no response was received
-                  console.log(error.request);
+                    // The request was made but no response was received
+                    console.log(error.request);
                 } else {
                     console.log("Inside error.Message");
-                  // Something happened in setting up the request that triggered an Error
-                  console.log('Error', error.message);
+                    // Something happened in setting up the request that triggered an Error
+                    console.log('Error', error.message);
                 }
-            })
+            }).finally(
+                response => {
+                    console.log("I'm at the finally part")
+                    console.log("the header : " + response.headers)
+                    console.log("the Message : " + response.message)
+                }
+
+            )
     }, [getDeviceInformationUrl]);
     return (deviceinformations);
 
